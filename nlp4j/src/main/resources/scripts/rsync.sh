b990524baff217a551e40497ac749b06f4706662
jdchoi77 <jinho.choi@emory.edu>
2015-10-24 21:55:16 -0400

    Added rsync.

--- nlp4j/src/main/resources/scripts/rsync.sh
+++ nlp4j/src/main/resources/scripts/rsync.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 jar cf emorynlp.jar$1 edu
 rsync -avc emorynlp.jar jdchoi@ainos.mathcs.emory.edu:/home/jdchoi/lib
+#rsync -avc emorynlp.jar choi@lab0z.mathcs.emory.edu:/home/choi/lib
+#scp choi@lab0z.mathcs.emory.edu:/home/choi/lib/emorynlp.jar jdchoi@ainos.mathcs.emory.edu:/home/jdchoi/lib/emorynlp.jar
