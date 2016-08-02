e96167dbb36223f4ad98b83ffbaf8daad4aa6d92
Jinho Choi <choi@mathcs.emory.edu>
2016-08-01 20:10:17 -0400

    Updated webpage.

---
+++ constant.js
@@ -0,0 +1,15 @@
+const HEADER = '<h1 class="project-name">NLP4J</h1>\
+      <h2 class="project-tagline"><b>NLP Toolkit for JVM Languages</b></h3>\
+      <a href="https://github.com/emorynlp/nlp4j" target="_blank" class="btn">View on GitHub</a>\
+      <a href="nlp.mathcs.emory.edu:8080/nlp4j" target="_blank" class="btn">View our Demo</a>\
+      <a href="http://nlp.mathcs.emory.edu" target="_blank" class="btn">Visit Emory NLP</a>';
+
+const HEADER2 = '<a href="https://emorynlp.github.io/nlp4j"><h1 class="project-name"><font color="white">NLP4J</font></h1></a>';
+
+const VERSION = '1.1.3';
+const URL_ASSEMBLER = '<a href="http://nlp.mathcs.emory.edu/nlp4j/nlp4j-appassembler-'+VERSION+'.tgz">'
+
+
+const FOOTER = '<span class="site-footer-credits">&copy;'+new Date().getFullYear()+' by <a href="http://nlp.mathcs.emory.edu">Emory NLP</a>. All Rights Reserved.</span>'
+
+
